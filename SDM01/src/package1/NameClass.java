4404387262789a936015109c32ede4d73137cfce
Mohit Rajpurohit <110395652+Mohit11695@users.noreply.github.com>
2022-08-13 14:04:55 +0530

    Update NameClass.java
    LIne number 7 and 8 is updatd

--- SDM01/src/package1/NameClass.java
+++ SDM01/src/package1/NameClass.java
@@ -2,10 +2,12 @@ package package1;
 
 public class NameClass {
 
-	public static void main(String[] args)
-	{
-		System.out.println("This is name of Murlidhar Hambarde class");
-		System.out.println("Amol ");
+	public static void main(String[] args) {
+		{
+			System.out.println("This is name of Murlidhar Hambarde class");
+			System.out.println("Amol ");
+
+		}
 
 	}
 
